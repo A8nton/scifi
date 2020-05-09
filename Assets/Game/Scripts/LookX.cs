c0914183e8be1f5462689e2b6e7423ce1bdd2d43
A8nton <toskagg01@gmail.com>
2020-05-09 16:49:19 +0300

    init commit

---
+++ Assets/Game/Scripts/LookX.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LookX : MonoBehaviour {
+
+    [SerializeField]
+    private float _sensitivity = 2f;
+
+    void Update() {
+        float _mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 angles = transform.localEulerAngles;
+        angles.y += _mouseX * _sensitivity;
+        transform.localEulerAngles = angles;
+    }
+}
---
+++ Assets/Game/Scripts/LookY.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LookY : MonoBehaviour {
+
+    [SerializeField]
+    private float _sensitivity = 2f;
+
+    void Update() {
+        float _mouseY = Input.GetAxis("Mouse Y") * -1;
+
+        Vector3 angles = transform.localEulerAngles;
+        angles.x += _mouseY * _sensitivity;
+        transform.localEulerAngles = angles;
+    }
+}
