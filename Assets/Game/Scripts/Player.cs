51a5610a9581849cf328ceaacc92522ad6c7f03e
A8nton <toskagg01@gmail.com>
2020-05-10 21:38:49 +0300

    Fire effect implemented

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -9,21 +9,32 @@ public class Player : MonoBehaviour {
     [SerializeField]
     private float _speed = 3f;
     private float _gravity = 9.81f;
+    [SerializeField]
+    private GameObject _muzzleFire;
+    [SerializeField]
+    private GameObject _hitMarkerPrefab;
 
     void Start() {
+        _muzzleFire.SetActive(false);
         _controller = GetComponent<CharacterController>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update() {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButton(0)) {
+            _muzzleFire.SetActive(true);
             Ray rayOrign = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
             RaycastHit hitInfo;
 
             if (Physics.Raycast(rayOrign, out hitInfo)) {
                 Debug.Log("Hit: " + hitInfo.transform.name);
+                GameObject hitMarker = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                Destroy(hitMarker, 0.3f);
             }
+        }
+        else {
+            _muzzleFire.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Escape)) {
@@ -36,5 +47,5 @@ public class Player : MonoBehaviour {
 
         velocity = transform.transform.TransformDirection(velocity);
         _controller.Move(velocity * Time.deltaTime);
-    }
+    } 
 }
