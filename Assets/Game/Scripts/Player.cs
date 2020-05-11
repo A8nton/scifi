e6ef3ad287b3ae907b60011a208874ea57bee570
A8nton <toskagg01@gmail.com>
2020-05-11 20:18:51 +0300

    Weapon reload and animation done

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -15,28 +15,31 @@ public class Player : MonoBehaviour {
     private GameObject _hitMarkerPrefab;
     [SerializeField]
     private AudioSource _audioSource;
+    [SerializeField]
+    private int _currentAmmo;
+    private int _maxAmmo = 100;
+    [SerializeField]
+    private int _totalAmmo;
+    private bool _canFire;
+    private UiManager _uiManager;
+    [SerializeField]
+    private Animator _animator;
 
     void Start() {
         _muzzleFire.SetActive(false);
         _controller = GetComponent<CharacterController>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        _currentAmmo = _maxAmmo;
+        _totalAmmo = 1000;
+        _canFire = true;
+        _uiManager = GameObject.Find("Canvas").GetComponent<UiManager>();
+        _uiManager.UpdateAmmo(_currentAmmo, 1000);
     }
 
     void Update() {
-        if (Input.GetMouseButton(0)) {
-            if (!_audioSource.isPlaying) {
-                _audioSource.Play();
-            }
-            _muzzleFire.SetActive(true);
-            Ray rayOrign = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            RaycastHit hitInfo;
-
-            if (Physics.Raycast(rayOrign, out hitInfo)) {
-                Debug.Log("Hit: " + hitInfo.transform.name);
-                GameObject hitMarker = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
-                Destroy(hitMarker, 0.3f);
-            }
+        if (Input.GetMouseButton(0) && _currentAmmo > 0) {
+            Shoot();
         }
         else {
             _muzzleFire.SetActive(false);
@@ -53,5 +56,41 @@ public class Player : MonoBehaviour {
 
         velocity = transform.transform.TransformDirection(velocity);
         _controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.R) && _totalAmmo > 0) {
+            StartCoroutine(ReloadCoroutine());
+        }
+    }
+
+    void Shoot() {
+        if (_canFire) {
+            _currentAmmo--;
+            if (!_audioSource.isPlaying) {
+                _audioSource.Play();
+            }
+            _muzzleFire.SetActive(true);
+            Ray rayOrign = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitInfo;
+            _uiManager.UpdateAmmo(_currentAmmo, _totalAmmo);
+
+            if (Physics.Raycast(rayOrign, out hitInfo)) {
+                Debug.Log("Hit: " + hitInfo.transform.name);
+                GameObject hitMarker = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                Destroy(hitMarker, 0.3f);
+            }
+        }
+    }
+
+    IEnumerator ReloadCoroutine() {
+        _animator.Play("Reload_anim");
+        _canFire = false;
+        int neededAmmo = _maxAmmo - _currentAmmo;
+
+        _currentAmmo = _currentAmmo + Mathf.Min(neededAmmo, _totalAmmo);
+        _totalAmmo = Mathf.Max(_totalAmmo - neededAmmo, 0);
+
+        yield return new WaitForSeconds(2);
+        _uiManager.UpdateAmmo(_currentAmmo, _totalAmmo);
+        _canFire = true;
     }
 }
---
+++ Assets/Game/Scripts/UiManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour{
+
+    [SerializeField]
+    private Text _ammoText;
+
+    public void UpdateAmmo(int count, int total) {
+        _ammoText.text = string.Format("Ammo: {0} / {1}", count, total);
+    }
+}
